526c694f24f5b0bdf07d9847d54260f51b5464e0
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-11-28 21:31:06 +0300

    Install NPM

--- src/components/Animatronix.js
+++ src/components/Animatronix.js
@@ -1,4 +1,4 @@
-class Animatronix {
+export default class Animatronix {
   constructor({
     type,
     el,
@@ -226,18 +226,3 @@ class Animatronix {
     }, this._delay + 1);
 	}
 }
-
-const title = document.querySelector(".richText__title");
-const playBtn = document.querySelector(".button__play");
-const line = document.querySelector(".richText__separator");
-
-const myObj = {
-  el: title,
-  type: "fadeInUp",
-  timeout: 3000,
-  delay: 2000,
-  handler: playBtn,
-	easing: 'ease-in-out',
-};
-const myTestObj = new Animatronix(myObj);
-myTestObj.setHandlers();
