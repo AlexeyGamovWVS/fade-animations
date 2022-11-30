6d10f406cff4a253c141cf0321da0390c3593fbf
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-11-30 20:46:16 +0300

    feat: завершить функционал анимаций и сделать рефакторинг

---
+++ src/components/gui.js
@@ -0,0 +1,43 @@
+import * as dat from "dat.gui";
+
+export default function generateGui(state) {
+  const gui = new dat.GUI();
+  const titleFolder = gui.addFolder("Title Animation");
+  const lineFolder = gui.addFolder("Line Animation");
+  const descFolder = gui.addFolder("Description Animation");
+  const imgFolder = gui.addFolder("Image Animation");
+
+  addOptionsFolder(titleFolder, state.title);
+  addOptionsFolder(lineFolder, state.line);
+  addOptionsFolder(descFolder, state.description);
+  addOptionsFolder(imgFolder, state.img);
+}
+
+function addOptionsFolder(folder, stateItem) {
+  folder.add(stateItem, "type", {
+    "fade-In": "fadeIn",
+    "fade-In-Up": "fadeInUp",
+    "fade-In-Left": "fadeInLeft",
+    "fade-In-Right": "fadeInRight",
+    "fade-In-Down": "fadeInDown",
+    "fade-Out": "fadeOut",
+    "fade-Out-Left": "fadeOutLeft",
+    "fade-Out-Right": "fadeOutRight",
+    "fade-Out-Up": "fadeOutUp",
+    "fade-Out-Down": "fadeOutDown",
+    "fade-In-Size-Center": "fadeInSizeCenter",
+    "fade-In-Size-Left": "fadeInSizeLeft",
+    "fade-In-Size-Right": "fadeInSizeRight",
+  });
+
+  folder.add(stateItem, "duration", 0.1, 3, 0.1);
+  folder.add(stateItem, "distance", 10, 1000, 10);
+  folder.add(stateItem, "easing", {
+    "ease": "power1.",
+    "ease-in": "power1.in",
+    "ease-out": "power1.out",
+    "ease-in-out": "power1.inOut",
+  });
+
+  folder.add(stateItem, "delay", 0, 10, 0.1);
+}
--- src/index.js
+++ src/index.js
@@ -1,61 +1,19 @@
 import "./style.css";
-import * as dat from "dat.gui";
-import { getAnimation } from "./getAnimation";
+import { getAnimation } from "./components/getAnimation";
+import { UI, initialState } from "./utils/data";
+import generateGui from "./components/gui";
 
-const UI = {
-  BUTTON_PLAY: document.querySelector(".button__play"),
-  BUTTON_GENERATE: document.querySelector(".generate"),
-  BUTTON_COPY: document.querySelector(".copy"),
-  OUTPUT: document.querySelector(".output"),
-
-  TITLE: document.querySelector(".richText__title"),
-  LINE: document.querySelector(".richText__separator"),
-  DESCRIPTION: document.querySelector(".rishText__subtitle"),
-  IMG: document.querySelector(".richText__img"),
-};
-
-const state = {
-  title: {
-    type: "fadeIn",
-    duration: 1,
-    distance: 100,
-    easing: "ease-in",
-    delay: 0,
-  },
-  description: {
-    type: "fadeIn",
-    duration: 1,
-    distance: 100,
-    easing: "ease-in",
-    delay: 1,
-  },
-  img: {
-    type: "fadeIn",
-    duration: 1,
-    distance: 100,
-    easing: "ease-in",
-    delay: 2,
-  },
-  line: {
-    type: "fadeIn",
-    duration: 1,
-    distance: 100,
-    easing: "ease-in",
-    delay: 3,
-  },
-};
-
-generateGui(state);
+generateGui(initialState);
 
 UI.BUTTON_PLAY.addEventListener("click", playAnimation);
 UI.BUTTON_GENERATE.addEventListener("click", generateOutput);
 UI.BUTTON_COPY.addEventListener("click", copyOutput);
 
 function playAnimation() {
-  playAnimationElement(UI.TITLE, state.title);
-  playAnimationElement(UI.DESCRIPTION, state.description);
-  playAnimationElement(UI.LINE, state.line);
-  playAnimationElement(UI.IMG, state.img);
+  playAnimationElement(UI.TITLE, initialState.title);
+  playAnimationElement(UI.DESCRIPTION, initialState.description);
+  playAnimationElement(UI.LINE, initialState.line);
+  playAnimationElement(UI.IMG, initialState.img);
 }
 
 function playAnimationElement(element, stateObj) {
@@ -63,54 +21,11 @@ function playAnimationElement(element, stateObj) {
   animate(element, stateObj);
 }
 
-function generateGui(state) {
-  const gui = new dat.GUI();
-  const titleFolder = gui.addFolder("Title Animation");
-  const lineFolder = gui.addFolder("Line Animation");
-  const descFolder = gui.addFolder("Description Animation");
-  const imgFolder = gui.addFolder("Image Animation");
-
-  addOptionsFolder(titleFolder, state.title);
-  addOptionsFolder(lineFolder, state.line);
-  addOptionsFolder(descFolder, state.description);
-  addOptionsFolder(imgFolder, state.img);
-}
-
-function addOptionsFolder(folder, stateObj) {
-  folder.add(stateObj, "type", {
-    "fade-In": "fadeIn",
-    "fade-In-Up": "fadeInUp",
-    // "fade-In-Right": "fadeInRight",
-    // "fade-In-Bottom": "fadeInBottom",
-    // "fade-Out": "fadeOut",
-    // "fade-Out-Left": "fadeOutLeft",
-    // "fade-Out-Right": "fadeOutRight",
-    // "fade-Out-Up": "fadeOutUp",
-    // "fade-Out-Bottom": "fadeOutBottom",
-    // "fade-In-Size-Center": "fadeInSizeCenter",
-    // "fade-In-Size-Left": "fadeInSizeLeft",
-    // "fade-In-Size-Right": "fadeInSizeRight",
-  });
-
-  folder.add(stateObj, "duration", 0.1, 3, 0.1);
-
-  folder.add(stateObj, "distance", 10, 1000, 10);
-
-  folder.add(stateObj, "easing", {
-    ease: "power1.",
-    "ease-in": "power1.in",
-    "ease-out": "power1.out",
-    "ease-in-out": "power1.inOut",
-  });
-
-  folder.add(stateObj, "delay", 0, 10000, 100);
-}
-
 function generateOutput() {
-  const json = JSON.stringify(state);
-  UI.OUTPUT.textContent = json;
+  const text = JSON.stringify(initialState);
+  UI.OUTPUT.textContent = text;
 }
 
 function copyOutput() {
-  navigator.clipboard.writeText(JSON.stringify(state));
+  navigator.clipboard.writeText(JSON.stringify(initialState));
 }
